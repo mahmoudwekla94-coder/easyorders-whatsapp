93506412f938c3533d5744caaecfd2d122433414
mahmoudwekla94-coder <mahmoudwekla94@gmail.com>
2025-12-06 20:29:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 app.get("/", (req, res) => {
   res.send("WhatsApp EasyOrders Bot is running!");
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
